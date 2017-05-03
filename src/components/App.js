ea2cfde109513eb551948bedc02585feb47852a0
kevinkiklee <kevin.kik.lee@gmail.com>
2017-05-03 15:35:05 -0400

    Install styled-components

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import logo from '../assets/logo.svg';
 import '../styles/reset.css';
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <AppWrapper>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>React/Redux Boilerplate</h2>
+      </AppWrapper>
     );
   }
 }
