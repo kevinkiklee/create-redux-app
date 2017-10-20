15e48eaf654e61ba660ff885993aa90cf778c352
kevinkiklee <kevin.kik.lee@gmail.com>
2017-10-19 21:04:47 -0400

    Remove styled-components

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const DataWrapper = styled.section`
-  font-size: 16px;
-  width: 1000px;
-  margin: 25px auto;
-
-  h4 {
-    padding: 5px;
-  }
-`;
 
 const Data = ({ quotes }) => {
   return (
-    <DataWrapper>
-      { quotes.map((quote, i) => <h4 key={i}>{quote}</h4>) }
-    </DataWrapper>
+    <div className='data'>
+      {quotes.map((quote, i) => <h4 key={i}>{quote}</h4>)}
+    </div>
   );
 };
 
--- src/components/Page/Header.js
+++ src/components/Page/Header.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
-
 import logo from '../../assets/logo.svg';
-
-const HeaderWrapper = styled.section`
-  h1 {
-    font-size: 20px;
-  }
-
-  .AppLogo {
-    height: 80px;
-  }
-`;
 
 const Header = () => {
   return (
-    <HeaderWrapper>
+    <div className='header'>
       <img src={logo} className="AppLogo" alt="logo" />
       <h1>React/Redux Boilerplate</h1>
-    </HeaderWrapper>
+    </div>
   );
 };
 
